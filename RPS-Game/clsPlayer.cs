1fabbc9d0f648b32a0564d769e4eec13b8bbd640
99Muhammad <MuhammadIbraheem99m@gmail.com>
2024-06-24 11:16:37 +0300

    kjdl

---
+++ RPS-Game/clsPlayer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab4_Rock__Paper__Scissors
+{
+    public class clsPlayer
+    {
+
+        private static string PlayerName;
+        private int PlayerScore;
+        private int AI_Score;
+
+        Random random = new Random();
+        public enum enPlayerChoosing { paper = 1, rock = 2, scissors = 3 }
+
+        public static enPlayerChoosing AI_Choise;
+        public static enPlayerChoosing playerChoosing;
+
+        public static string playerName { get { return PlayerName; } set { PlayerName = value; } }
+        public int playerScore { get { return PlayerScore; } set { PlayerScore = value; } }
+
+        public int Ai_Score { get { return AI_Score; } set { AI_Score = value; } }
+
+
+        public static enPlayerChoosing GetPlayerChoosing()
+        {
+            Console.Write("\t\t[1] {0,-10} [2] {1,-10} [3] {2,-10}", "Paper", "Rock", "Scissors" +
+               "\n\t\tWtite one of three choises or write from 1 - 3 ?");
+
+            /*
+        
+             * When the text lengths are not equal, the tab characters won't align the text correctly,
+             * and the output may not look as expected.
+            
+              Console.WriteLine("\t\t[1]Paper\t[2]Rock\t[3]Scissors\n\t\tWtite one of three choises" +
+              "or write from 1 - 3 ?");
+
+            Alternatively, you can use the PadRight() method to achieve the same result:
+
+            Console.WriteLine("[1] {0} [2] {1} [3] {2}", "Paper".PadRight(10), 
+            "Rock".PadRight(10), "Scissors".PadRight(10));
+
+             */
+
+
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                Console.Write("");
+                string userInput = Console.ReadLine().Trim().ToUpper();
+
+
+                if (Enum.TryParse(userInput, true, out playerChoosing))
+                {
+                    // this if condition to prevent player to enter any number rahter than 1,2,3 
+                    if (playerChoosing == enPlayerChoosing.paper || playerChoosing == enPlayerChoosing.scissors ||
+                        playerChoosing == enPlayerChoosing.rock)
+                    {
+                        validInput = true;
+                        return playerChoosing;
+                    }
+                    else
+                    {
+                        Console.Write("\t\tInvalid input. Please try again: ");
+                    }
+                }
+                else
+                {
+                    Console.Write("\t\tInvalid input. Please try again: ");
+                }
+            }
+            return playerChoosing;
+
+        }
+
+        public static enPlayerChoosing GetRandomChoising()
+        {
+            Random random = new Random();
+
+            enPlayerChoosing[] arrAiChoising = new enPlayerChoosing[]
+            { enPlayerChoosing.paper,enPlayerChoosing.rock,enPlayerChoosing.scissors};
+
+            int RandomNumber = random.Next(1, 3);
+
+            AI_Choise = arrAiChoising[RandomNumber];
+            return AI_Choise;
+
+        }
+    }
+}
---
+++ RPS-Game/clsRPSGame.cs
@@ -0,0 +1,158 @@
+﻿using static Lab4_Rock__Paper__Scissors.clsPlayer;
+
+namespace Lab4_Rock__Paper__Scissors
+{
+    public class clsRPSGame
+    {
+        clsPlayer Player = new clsPlayer();
+
+        public void StartGameScreen()
+        {
+            Console.WriteLine("\t\t--------------------------------------\n");
+            Console.WriteLine("\t\t\tRock , Paper , Sissors Game Screen\n");
+            Console.WriteLine("\t\t--------------------------------------\n");
+
+            Console.Write("\t\tEnter Your Name ,please?");
+            clsPlayer.playerName = Console.ReadLine();
+
+            while (int.TryParse(clsPlayer.playerName, out int number))
+
+            {
+                Console.WriteLine("\t\tYou entered invalid data ,try again please");
+                Console.Write("\t\tEnter Your Name ,please?");
+                clsPlayer.playerName = Console.ReadLine();
+
+            }
+            Player.playerScore = 0;
+        }
+
+        public void UpdateScore(ref short playerScore, ref short AiScore)
+        {
+            playerScore = 0;
+            AiScore = 0;
+
+            if (playerChoosing == AI_Choise)
+            {
+                playerScore = 0;
+                AiScore = 0;
+            }
+            else if (playerChoosing == enPlayerChoosing.rock && AI_Choise == enPlayerChoosing.scissors)
+            {
+                ++Player.playerScore;
+                ++playerScore;
+            }
+            else if (playerChoosing == enPlayerChoosing.scissors && AI_Choise == enPlayerChoosing.paper)
+            {
+                ++Player.playerScore;
+                ++playerScore;
+            }
+            else if (playerChoosing == enPlayerChoosing.paper && AI_Choise == enPlayerChoosing.rock)
+            {
+                ++Player.playerScore;
+                ++playerScore;
+            }
+            else
+            {
+                ++Player.Ai_Score;
+                ++AiScore;
+            }
+
+        }
+
+        public void ResultForEachRound(short playerScore, short AiScore)
+        {
+            Console.WriteLine("\n\t\t{ Round Info }");
+            Console.WriteLine($"\n\t\t-The choise for Player {clsPlayer.playerName}: {clsPlayer.playerChoosing} ");
+            Console.WriteLine($"\t\t-The choise for AI Player : {clsPlayer.AI_Choise} ");
+            Console.WriteLine($"\t\t-The score for Player {clsPlayer.playerName} : {playerScore}");
+            Console.WriteLine($"\t\t-The score for AI Player : {AiScore}");
+        }
+
+        public string WhoseCurrentWinner(short playerScore, short AiScore)
+        {
+            if (playerScore > AiScore)
+            {
+                ResultForEachRound(playerScore, AiScore);
+                Console.WriteLine($"\t\tPlayer {clsPlayer.playerName} win ");
+                return clsPlayer.playerName;
+            }
+            else if (playerScore < AiScore)
+
+            {
+                ResultForEachRound(playerScore, AiScore);
+                Console.WriteLine($"\t\tAi win ");
+                return "Ai Player";
+            }
+            else if (playerScore == AiScore)
+            {
+                ResultForEachRound(playerScore, AiScore);
+                Console.WriteLine($"\t\tNo winner ");
+                return "No winner";
+            }
+            return "";
+        }
+
+        public void ShowFinalResult()
+        {
+            Console.WriteLine("\n\t\t--------------------------------------\n");
+            Console.WriteLine($"\t\t\tFinal Result \n");
+            Console.WriteLine("\t\t--------------------------------------\n");
+            Console.WriteLine($"\t\tThe score for Player {clsPlayer.playerName} : {Player.playerScore} ");
+            Console.WriteLine($"\t\tThe score for AI Player : {Player.Ai_Score} ");
+
+        }
+        public void CountFinalResult()
+        {
+            if (Player.playerScore > Player.Ai_Score)
+            {
+                ShowFinalResult();
+                Console.WriteLine($"\t\tPlayer {clsPlayer.playerName} win ");
+            }
+            else if (Player.playerScore < Player.Ai_Score)
+
+            {
+                ShowFinalResult();
+                Console.WriteLine($"\t\tAi win ");
+            }
+            else
+            {
+                ShowFinalResult();
+                Console.WriteLine($"\t\tNo winner ");
+            }
+        }
+
+        public void ImplementSingleRound(ushort RoundNumber)
+        {
+
+            short playerScore = 0;
+            short AiScore = 0;
+            Console.WriteLine("\t\t--------------------------------------\n");
+            Console.WriteLine($"\t\t\tRound ( {RoundNumber} ) \n");
+            Console.WriteLine("\t\t--------------------------------------\n");
+
+            clsPlayer.playerChoosing = clsPlayer.GetPlayerChoosing();
+            clsPlayer.AI_Choise = clsPlayer.GetRandomChoising();
+
+            UpdateScore(ref playerScore, ref AiScore);
+            WhoseCurrentWinner(playerScore, AiScore);
+
+
+        }
+
+        public void Rounds(ushort RoundNumber = 0)
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t--------------------------------------\n");
+            Console.WriteLine("\t\t\tRounds Screen \n");
+            Console.WriteLine("\t\t--------------------------------------\n\n\n");
+
+
+            while (++RoundNumber <= 3)
+            {
+                ImplementSingleRound(RoundNumber);
+            }
+            CountFinalResult();
+
+        }
+    }
+}
